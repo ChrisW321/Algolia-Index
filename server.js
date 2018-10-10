421581def6315c5e57e38a6f3ce73929982891b3
Chris W <christopherkwildenradt@gmail.com>
2018-10-10 12:39:04 -0700

    Copy over setup from personal quick react repo

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 5646;
+
+app.use('/', express.static('public'));
+
+app.listen(port, () => console.log(`Lisening on http://127.0.0.1:${port}`));
